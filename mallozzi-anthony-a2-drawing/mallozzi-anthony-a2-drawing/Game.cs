b5a3666a85f22dc9a9edb75b31df8d8a2eee4276
Stick <Stick@GEOFF>
2025-10-02 13:09:52 -0400

    Changed Rune Colors and wasted time trying to get some other stuff working.

--- mallozzi-anthony-a2-drawing/mallozzi-anthony-a2-drawing/Game.cs
+++ mallozzi-anthony-a2-drawing/mallozzi-anthony-a2-drawing/Game.cs
@@ -12,9 +12,13 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
+
         ColorF bgColor = new ColorF(0.3f, 0.1f, 0.1f);
         ColorF symColor = new ColorF(0.8f, 0.2f, 0.1f);
         ColorF shelfColor = new ColorF(0.5f, 0.3f, 0.2f);
+        ColorF runeColorA = new ColorF(1.0f, 0.0f, 0.9f);
+        ColorF runeColorB = new ColorF(0.0f, 0.9f, 1.0f);
+        ColorF runeColorC = new ColorF(0.25f, 1.0f, 0.0f);
 
         float playerY;
         float playerX;
@@ -24,9 +28,9 @@ namespace MohawkGame2D
 
         float rectanglePosition;
 
-        bool keyInputA;
-        bool keyInputB;
-        bool keyInputC;
+        bool aVisibility = false;
+        bool bVisibility = false;
+        bool cVisibility = false;
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
@@ -37,12 +41,17 @@ namespace MohawkGame2D
             Window.SetSize(400, 400);
             Window.TargetFPS = 60;
         }
+
+        //Backgound Gaps function
+
         public void bgGapsFunc()
         {
             Draw.LineSize = 4;
             Draw.LineColor = Color.Black;
             Draw.Line(0, gapsY1, 500 - 50, gapsY2);
         }
+
+        //triangle symbol function
 
         public void symbolTriangle()
         {
@@ -56,6 +65,9 @@ namespace MohawkGame2D
             Draw.FillColor = Color.Clear;
             Draw.Triangle(200 - playerX, 45 - playerY, 80 - playerX, 300 - playerY, 320 - playerX, 300 - playerY);
         }
+
+        //circle symbol function
+
         public void symbolCircle()
         {
             Draw.LineSize = 15;
@@ -68,6 +80,9 @@ namespace MohawkGame2D
             Draw.FillColor = Color.Clear;
             Draw.Circle(200 - playerX, 200 - playerY, 150);
         }
+
+        //shelf function
+
         public void bottomItems()
         {
             Draw.LineSize = 5;
@@ -75,22 +90,111 @@ namespace MohawkGame2D
             Draw.FillColor = shelfColor;
             Draw.Rectangle(0, rectanglePosition, 400, 100);
         }
+        public void letterA()
+        {
+            Draw.LineSize = 5;
+            Draw.LineColor = Color.Black;
+            Draw.Line(130 - playerX, 480 - playerY, 150 - playerX, 420 - playerY);
+            Draw.Line(150 - playerX, 420 - playerY, 180 - playerX, 480 - playerY);
+        }
+        public void letterB()
+        {
+            Draw.LineSize = 5;
+            Draw.LineColor = Color.Black;
+            Draw.Line(170 - playerX, 450 - playerY, 210 - playerX, 460 - playerY);
+            Draw.Line(210 - playerX, 460 - playerY, 180 - playerX, 480 - playerY);
+            Draw.Line(180 - playerX, 480 - playerY, 170 - playerX, 420 - playerY);
+            Draw.Line(170 - playerX, 420 - playerY, 210 - playerX, 440 - playerY);
+            Draw.Line(210 - playerX, 440 - playerY, 170 - playerX, 460 - playerY);
+        }
+        public void letterC()
+        {
+            Draw.LineSize = 5;
+            Draw.LineColor = Color.Black;
+            Draw.Line(250 - playerX, 460 - playerY, 230 - playerX, 480 - playerY);
+            Draw.Line(230 - playerX, 480 - playerY, 210 - playerX, 450 - playerY);
+            Draw.Line(210 - playerX, 450 - playerY, 230 - playerX, 420 - playerY);
+            Draw.Line(230 - playerX, 420 - playerY, 250 - playerX, 440 - playerY);
+        }
+
+        //symbol A function
+
+        public void ritualCircleOne()
+        {
+            Draw.LineSize = 12.0f;
+            Draw.LineColor = Color.Black;
+            Draw.Line(180 - playerX, 40 - playerY, 210 - playerX, 20 - playerY);
+            Draw.Line(210 - playerX, 20 - playerY, 210 - playerX, 70 - playerY);
+
+            Draw.LineSize = 5.0f;
+            Draw.LineColor = runeColorA;
+            Draw.Line(180 - playerX, 40 - playerY, 210 - playerX, 20 - playerY);
+            Draw.Line(210 - playerX, 20 - playerY, 210 - playerX, 70 - playerY);
+
+        }
+
+        //symbol B function
+
+        public void ritualCircleTwo()
+        {
+            Draw.LineSize = 12.0f;
+            Draw.LineColor = Color.Black;
+            Draw.Line(300 - playerX, 320 - playerY, 330 - playerX, 260 - playerY);
+            Draw.Line(330 - playerX, 260 - playerY, 340 - playerX, 300 - playerY);
+            Draw.Line(340 - playerX, 300 - playerY, 300 - playerX, 280 - playerY);
+
+            Draw.LineSize = 5.0f;
+            Draw.LineColor = runeColorB;
+            Draw.Line(300 - playerX, 320 - playerY, 330 - playerX, 260 - playerY);
+            Draw.Line(330 - playerX, 260 - playerY, 340 - playerX, 300 - playerY);
+            Draw.Line(340 - playerX, 300 - playerY, 300 - playerX, 280 - playerY);
+        }
+
+        //symbol C function
+
+        public void ritualCircleThree()
+        {
+            Draw.LineSize = 12.0f;
+            Draw.LineColor = Color.Black;
+            Draw.Line(80 - playerX, 300 - playerY, 80 - playerX, 280 - playerY);
+            Draw.Line(80 - playerX, 280 - playerY, 100 - playerX, 280 - playerY);
+            Draw.Line(100 - playerX, 280 - playerY, 70 - playerX, 310 - playerY);
+            Draw.Line(70 - playerX, 310 - playerY, 90 - playerX, 310 - playerY);
+            Draw.Line(90 - playerX, 310 - playerY, 90 - playerX, 290 - playerY);
+
+            Draw.LineSize = 5.0f;
+            Draw.LineColor = runeColorC;
+            Draw.Line(80 - playerX, 300 - playerY, 80 - playerX, 280 - playerY);
+            Draw.Line(80 - playerX, 280 - playerY, 100 - playerX, 280 - playerY);
+            Draw.Line(100 - playerX, 280 - playerY, 70 - playerX, 310 - playerY);
+            Draw.Line(70 - playerX, 310 - playerY, 90 - playerX, 310 - playerY);
+            Draw.Line(90 - playerX, 310 - playerY, 90 - playerX, 290 - playerY);
+
+        }
 
         ///     Update runs every frame.
         /// </summary>
         public void Update()
         {
+            
             Window.ClearBackground(bgColor);
 
-            rectanglePosition = Input.GetMouseY() * -0.3f + 425;
+            //declare and initialize keyboard inputs
 
-            keyInputA = Input.IsKeyboardKeyDown(KeyboardInput.A);
-            
+            bool keyInputA = Input.IsKeyboardKeyDown(KeyboardInput.A);
 
+            bool keyInputB = Input.IsKeyboardKeyDown(KeyboardInput.B);
 
+            bool keyInputC = Input.IsKeyboardKeyDown(KeyboardInput.C);
+
+            bool[] keyInputArray = { keyInputA, keyInputB, keyInputC };
+
+            //declare player mouse position
 
             playerY = Input.GetMouseY() * 0.4f - 50;
             playerX = Input.GetMouseX() * 0.4f - 50;
+
+            //creat backround gaps using loop and function
 
             for (float i = 0; i < 16; i++)
             {
@@ -99,17 +203,58 @@ namespace MohawkGame2D
                 bgGapsFunc();
             }
 
+            //place shapes
+
             symbolCircle();
 
             symbolTriangle();
 
-            bottomItems();
+            letterA();
+            letterB();
+            letterC();
 
-            for (int i = 0; i <= 3; i++)
+            //set keys to symbol appearances
+
+            if (keyInputArray[0] == true)
             {
-                if (keyInput)
+                aVisibility = true;
+                bVisibility = false;
+                cVisibility = false;
+                Console.WriteLine("ritual A selected");
+            }
+            else if (keyInputArray[1] == true)
+            {
+                bVisibility = true;
+                aVisibility = false;
+                cVisibility = false;
+                Console.WriteLine("ritual B selected");
+            }
+            else if (keyInputArray[2] == true)
+            {
+                cVisibility = true;
+                aVisibility = false;
+                bVisibility = false;
+                Console.WriteLine("ritual C selected");
+            }
+            else if (Input.IsKeyboardKeyPressed(KeyboardInput.Space))
+            {
+
+            }
+
+            if (aVisibility == true)
+            {
+                ritualCircleOne();
+            }
+
+            if (bVisibility == true)
+            {
+                ritualCircleTwo();
+            }
+
+            if (cVisibility == true)
+            {
+                ritualCircleThree();
             }
         }
-
     }
 }
